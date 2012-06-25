45d5d907d1ab49c09a626faa11735f1fda59fef3
Brian Leathem <bleathem@gmail.com>
2012-06-24 23:30:18 -0700

    components lighting talk mind map

--- 2012-JUDCon/_prep/components_lightning.mm
+++ 2012-JUDCon/_prep/components_lightning.mm
@@ -14,13 +14,36 @@
 </node>
 <node CREATED="1340578096411" ID="ID_1456228731" MODIFIED="1340578103737" POSITION="right" TEXT="jQuery plugins">
 <node CREATED="1340578278010" ID="ID_1426821605" MODIFIED="1340578283817" TEXT="Lots of them out there">
-<node CREATED="1340578761547" ID="ID_534738666" MODIFIED="1340578773818" TEXT="JSF Component ~3.6 Million hits"/>
+<node CREATED="1340578761547" ID="ID_534738666" MODIFIED="1340579083933">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      JSF Component ~ 3 Million hits
+    </p>
+    <p>
+      javascript component ~ 416 million hits
+    </p>
+  </body>
+</html>
+</richcontent>
 </node>
 </node>
-<node CREATED="1340578105131" ID="ID_1974167017" MODIFIED="1340578114378" POSITION="right" TEXT="Wrap javascript widgets">
+</node>
+<node CREATED="1340578105131" ID="ID_1974167017" MODIFIED="1340579088515" POSITION="right" TEXT="Wrap javascript widgets">
 <node CREATED="1340578115803" ID="ID_748305157" MODIFIED="1340578119305" TEXT="Composite Components"/>
 <node CREATED="1340578119963" ID="ID_180514748" MODIFIED="1340578122826" TEXT="RichFaces CDK"/>
 </node>
-<node CREATED="1340578124123" ID="ID_576085544" MODIFIED="1340578157066" POSITION="right" TEXT="Why do I care"/>
+<node CREATED="1340578124123" ID="ID_576085544" MODIFIED="1340578157066" POSITION="right" TEXT="Why do I care">
+<node CREATED="1340579495806" ID="ID_1240739116" MODIFIED="1340579507100" TEXT="Polyglot JVM"/>
+<node CREATED="1340579507995" ID="ID_1616375053" MODIFIED="1340579515228" TEXT="Polyglot Web frameworks"/>
+<node CREATED="1340579517659" ID="ID_1505249235" MODIFIED="1340579542284" TEXT="Consistent L&amp;F"/>
+<node CREATED="1340579548091" ID="ID_429273043" MODIFIED="1340579551212" TEXT="Call to action">
+<node CREATED="1340579553531" ID="ID_1445551054" MODIFIED="1340579613180" TEXT="App builder incentive: Consistent l&amp;F across applications"/>
+<node CREATED="1340579614779" ID="ID_715604960" MODIFIED="1340579663929" TEXT="Framework Builders: Let&apos;s provide a consistent set of components"/>
+</node>
+</node>
 </node>
 </map>
