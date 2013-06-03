7a32c7dabd64870fa5cd59cc8c47b9ece21bb4ae
Brian Leathem <bleathem@gmail.com>
2013-06-03 08:11:26 -0700

    JAX-2013 draft

--- 2013-JAX/_ext/pipeline.rb
+++ 2013-JAX/_ext/pipeline.rb
@@ -1,5 +1,8 @@
+require File.join File.dirname(__FILE__), 'tweakruby'
+require_relative "copy_files.rb"
 
 Awestruct::Extensions::Pipeline.new do
+  extension Awestruct::Extensions::CopyFiles.new
   # extension Awestruct::Extensions::Posts.new( '/news' ) 
   # extension Awestruct::Extensions::Indexifier.new
 end
