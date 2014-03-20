afe55254a740a8d8f4fbe7b20129735a203df563
Brian Leathem <bleathem@gmail.com>
2014-03-20 14:29:43 -0700

    Progress on the DevNation jQuery UI Widget factory talk

--- asciidoctor-backends/slim/dzslides/helpers.rb
+++ asciidoctor-backends/slim/dzslides/helpers.rb
@@ -1,3 +1,4 @@
+require 'asciidoctor/backends/_stylesheets'
 # Add custom functions to this module that you want to use in your Slim
 # templates. Within the template you must namespace the function
 # (unless someone can show me how to include them in the evaluation context).
