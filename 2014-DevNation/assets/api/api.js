f4a8cafebf2aeb8fee685eecc848947ed173da18
Brian Leathem <bleathem@gmail.com>
2014-04-02 15:22:36 -0700

    Added the richwidget API files

---
+++ 2014-DevNation/assets/api/api.js
@@ -0,0 +1,45 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "autocomplete",
+        "chart",
+        "dataScroller",
+        "dataTable",
+        "editor",
+        "orderingList",
+        "pickList",
+        "select",
+        "stopwatch",
+        "subTable",
+        "subTableToggle"
+    ],
+    "modules": [
+        "Input",
+        "Output",
+        "Select",
+        "Table"
+    ],
+    "allModules": [
+        {
+            "displayName": "Input",
+            "name": "Input",
+            "description": "Autocomplete enables users to quickly find and select from a pre-populated list of values as they type, leveraging searching and filtering.\n\nThe widget is based on popular jQuery UI Autocomplete widget, but it brings some advanced built-in features:\n\n* caching\n* tokenizing\n* using DOM as a source of suggestions\n* auto-fill\n* built-in button"
+        },
+        {
+            "displayName": "Output",
+            "name": "Output",
+            "description": "A widget for creating data chart"
+        },
+        {
+            "displayName": "Select",
+            "name": "Select",
+            "description": "A widget providing sorting capabilities for a list of elements"
+        },
+        {
+            "displayName": "Table",
+            "name": "Table",
+            "description": "A widget for paging through other iterating widgets"
+        }
+    ]
+} };
+});
